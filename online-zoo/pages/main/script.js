ff6b8f7d68927fd511134c2efd6170f4146a8ef9
SeregeyIankowski <garret933@gmail.com>
2022-09-26 12:46:20 +0300

    feat: add logo and email validation in footer main page

---
+++ online-zoo/pages/main/script.js
@@ -0,0 +1,15 @@
+const LOGO = document.querySelector('.logo');
+LOGO.addEventListener('click', () => window.open('../main/index.html', '_self').focus());
+
+const FOOTER_BUTTON = document.querySelector('.footer__subscribe-button');
+const FOOTER_INPUT = document.querySelector('.footer__subscribe-input');
+
+FOOTER_INPUT.oninput = (e) => {
+  const regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (e.target.value.match(regExp)) {
+    FOOTER_BUTTON.classList.remove('button_mistake');
+  } else {
+    FOOTER_BUTTON.classList.add('button_mistake');
+  }
+}
+
